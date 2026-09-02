9ae369144a4293ec9537812ecd01d98c81230fbd
jackko11150917 <jackko11150917@gmail.com>
2026-09-02 18:14:37 +0800

    fix: guide uses unified EXERCISES (includes machines)

--- src/routes/guide.tsx
+++ src/routes/guide.tsx
@@ -5,18 +5,11 @@ import { ExerciseIcon } from "@/components/exercise-icon";
 import { Input } from "@/components/ui/input";
 import {
   EQUIPMENT_LABELS,
-  EXERCISES as BASE_EXERCISES,
+  EXERCISES,
   MUSCLE_LABELS,
-  type Exercise,
   type MuscleGroup,
 } from "@/data/exercises";
-import { MACHINE_EXERCISES } from "@/data/machine-exercises";
 import { cn } from "@/lib/utils";
-
-const EXERCISES: Exercise[] = [
-  ...BASE_EXERCISES,
-  ...(MACHINE_EXERCISES as unknown as Exercise[]),
-];
 
 export const Route = createFileRoute("/guide")({ component: GuidePage });
 
